1103151e63f7d50ef4673457f134bff686340d52
Rastislav Wagner <rawagner@redhat.com>
2021-03-24 13:48:05 +0100

    Use exposed plugin api from window object

--- src/components/Foo.tsx
+++ src/components/Foo.tsx
@@ -1,5 +1,27 @@
 import * as React from 'react';
+// import { apis } from '@openshift-console/dynamic-plugin-sdk';
 
-const Foo: React.FC<{ label: string }> = ({ label }) => <h2>Hello {label} Component!</h2>;
+const Foo: React.FC = () => {
+    const { useK8sWatchResource } = (window as any).apis;
+    const [data, loaded, loadError] = useK8sWatchResource({
+      kind: 'Pod',
+      isList: true,
+      namespaced: true,
+    })
+
+    if (!loaded) {
+      return (
+        <div>loading</div>
+      )
+    }
+
+    if (loadError) {
+      return (
+        <div>error</div>
+      )
+    }
+
+    return <>{(data as any[]).map((d) => <div key={d.metadata.uid}>{d.metadata.name}</div>)}</>
+}
 
 export default Foo;
