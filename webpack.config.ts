91b65e8c6f0029c654fac29d7d2345c62eae3dec
Ian Bolton <ibolton@redhat.com>
2021-09-26 21:20:26 -0400

    fix webpack

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,49 +1,96 @@
 /* eslint-env node */
 
-import * as webpack from 'webpack';
-import * as path from 'path';
-import { ConsoleRemotePlugin } from '@openshift-console/dynamic-plugin-sdk/lib/index-webpack';
+import * as webpack from "webpack";
+import * as path from "path";
+import { ConsoleRemotePlugin } from "@openshift-console/dynamic-plugin-sdk/lib/index-webpack";
+const { stylePaths } = require("./stylePaths");
 
 const config: webpack.Configuration = {
-  mode: 'development',
-  context: path.resolve(__dirname, 'src'),
+  mode: "development",
+  context: path.resolve(__dirname, "src"),
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: '[name]-bundle.js',
-    chunkFilename: '[name]-chunk.js',
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name]-bundle.js",
+    chunkFilename: "[name]-chunk.js",
   },
   resolve: {
-    extensions: ['.ts', '.tsx', '.js', '.jsx'],
+    extensions: [".ts", ".tsx", ".js", ".jsx"],
   },
   module: {
     rules: [
+      {
+        test: /\.(jpg|jpeg|png|gif)$/i,
+        include: [
+          path.resolve(__dirname, "./src"),
+          path.resolve(__dirname, "./node_modules/patternfly"),
+          path.resolve(
+            __dirname,
+            "./node_modules/@patternfly/patternfly/assets/images"
+          ),
+          path.resolve(
+            __dirname,
+            "./node_modules/@patternfly/react-styles/css/assets/images"
+          ),
+          path.resolve(
+            __dirname,
+            "./node_modules/@patternfly/react-core/dist/styles/assets/images"
+          ),
+          path.resolve(
+            __dirname,
+            "./node_modules/@patternfly/react-core/node_modules/@patternfly/react-styles/css/assets/images"
+          ),
+          path.resolve(
+            __dirname,
+            "./node_modules/@patternfly/react-table/node_modules/@patternfly/react-styles/css/assets/images"
+          ),
+          path.resolve(
+            __dirname,
+            "./node_modules/@patternfly/react-inline-edit-extension/node_modules/@patternfly/react-styles/css/assets/images"
+          ),
+        ],
+        use: [
+          {
+            loader: "url-loader",
+            options: {
+              limit: 5000,
+              outputPath: "images",
+              name: "[name].[ext]",
+            },
+          },
+        ],
+        type: "javascript/auto",
+      },
       {
         test: /(\.jsx?)|(\.tsx?)$/,
         exclude: /node_modules/,
         use: [
           {
-            loader: 'ts-loader',
+            loader: "ts-loader",
             options: {
-              configFile: path.resolve(__dirname, 'tsconfig.json'),
+              configFile: path.resolve(__dirname, "tsconfig.json"),
             },
           },
         ],
       },
+      {
+        test: /\.css$/,
+        use: [{ loader: "style-loader" }, { loader: "css-loader" }],
+      },
     ],
   },
   plugins: [new ConsoleRemotePlugin()],
-  devtool: 'source-map',
+  devtool: "source-map",
   optimization: {
-    chunkIds: 'named',
+    chunkIds: "named",
     minimize: false,
   },
 };
 
-if (process.env.NODE_ENV === 'production') {
-  config.mode = 'production';
-  config.output.filename = '[name]-bundle-[hash].min.js';
-  config.output.chunkFilename = '[name]-chunk-[chunkhash].min.js';
-  config.optimization.chunkIds = 'deterministic';
+if (process.env.NODE_ENV === "production") {
+  config.mode = "production";
+  config.output.filename = "[name]-bundle-[hash].min.js";
+  config.output.chunkFilename = "[name]-chunk-[chunkhash].min.js";
+  config.optimization.chunkIds = "deterministic";
   config.optimization.minimize = true;
 }
 
